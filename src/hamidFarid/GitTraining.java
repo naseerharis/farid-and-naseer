4483456f021e77eb5447b409bdc0d9d51785512b
naseerharis <107784668+naseerharis@users.noreply.github.com>
2022-12-01 11:40:03 -0800

    Update GitTraining.java
    new change added

--- src/hamidFarid/GitTraining.java
+++ src/hamidFarid/GitTraining.java
@@ -4,7 +4,13 @@ public class GitTraining {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//this change from githup
+		int a = 150;
+		int b = 250;
+		int sum = a +b;
+		
 
 	}
+	
 
 }
